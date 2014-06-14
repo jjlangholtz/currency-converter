4c266d27852c729a834c25a2430d5bf2081b9378
Joshua Langholtz <jjlangholtz@gmail.com>
2014-06-14 13:34:49 -0400

    Currency converters are created with a hash of currency conversions

--- lib/currency_converter.rb
+++ lib/currency_converter.rb
@@ -1,2 +1,7 @@
 class CurrencyConverter
+  attr_reader :conversions
+
+  def initialize
+    @conversions = { USD: 1.0, EUR: 0.74 }
+  end
 end
--- test/currency_converter_test.rb
+++ test/currency_converter_test.rb
@@ -2,4 +2,8 @@ require 'minitest/autorun'
 require 'currency_converter'
 
 class CurrencyConverterTest < MiniTest::Unit::TestCase
+  def test_converter_should_have_codes_and_conversion_rates
+    @converter = CurrencyConverter.new
+    assert @converter.conversions
+  end
 end
