00cc7ce528d1ce66d54c01be06b7a242acd8c721
Joshua Langholtz <jjlangholtz@gmail.com>
2014-06-14 12:07:15 -0400

    Currency is created with an amount and a currency code

--- test/currency_test.rb
+++ test/currency_test.rb
@@ -2,13 +2,17 @@ require 'minitest/autorun'
 require 'currency'
 
 class CurrencyTest < MiniTest::Unit::TestCase
+  def setup
+    @currency = Currency.new(10, :USD)
+  end
+
   def test_currency_should_have_amount
-    amount = Currency.new(10, :USD).amount
+    amount = @currency.amount
     assert amount
   end
 
   def test_currency_should_have_code
-    code = Currency.new(10, :USD)
+    code = @currency.code
     assert code
   end
 end
