314cdc166ec0e87df4fc39abe68fd5a5ea444196
Joshua Langholtz <jjlangholtz@gmail.com>
2014-06-14 13:40:05 -0400

    Currency converter contains a hash with the correct conversion rates

--- test/currency_converter_test.rb
+++ test/currency_converter_test.rb
@@ -2,8 +2,15 @@ require 'minitest/autorun'
 require 'currency_converter'
 
 class CurrencyConverterTest < MiniTest::Unit::TestCase
-  def test_converter_should_have_codes_and_conversion_rates
+  def setup
     @converter = CurrencyConverter.new
+  end
+  def test_converter_should_have_codes_and_conversion_rates
     assert @converter.conversions
+  end
+
+  def test_converter_contains_correct_convertion_rates
+    assert_equal 1.0, @converter.conversions[:USD]
+    assert_equal 0.74, @converter.conversions[:EUR]
   end
 end
